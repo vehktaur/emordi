ac262735a6090eadd1040304af60d8cab04e4def
vehktaur <victorakhihiero@gmail.com>
2026-01-08 12:36:29 +0100

    update

--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -3,7 +3,7 @@ const NotFound = () => {
     <div className="flex flex-col items-center justify-center min-h-screen text-center px-5">
       <h1 className="clamp-[text,4xl,6xl] font-bold mb-4">404 - Not Found</h1>
       <p className="clamp-[text,lg,xl]">
-        The page you are looking for does not exist... yet?
+        The page you are looking for does not exist... yet.
       </p>
     </div>
   )
